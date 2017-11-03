e2f56ead38a23346bde29ad3bf213b0a4950be2c
luozx <remain@divergers.com>
2017-11-03 16:54:08 +0800

    finish the webpack-config

---
+++ src/client.js
@@ -0,0 +1,20 @@
+const a = 'a';
+console.log(a);
+
+import React from 'react';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {connect} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {routerReducer} from 'react-router-redux';
+import {renderRoutes} from 'react-router-config';
+
+const style = {
+  fontSize: '20px',
+  fontWeight: 'bold',
+};
+
+render(
+    <h1 style={{...style}}>hello</h1>,
+    document.getElementById('root'),
+);
