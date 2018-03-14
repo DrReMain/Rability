b2e24aa1aac774612488225be41a91864b6fc9fd
luozx <remain@divergers.com>
2018-03-14 17:29:55 +0800

    create new project

--- src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export * from './article'
-export * from './comment'
-export * from './other'
-export * from './auth'
--- src/config.js
+++
@@ -1,7 +0,0 @@
-export const API_ROOT = (process.env.NODE_ENV === 'production')
-  ? 'https://api.jackhu.top/'
-  : 'http://localhost:9000/'
-
-export const CookieDomain = (process.env.NODE_ENV === 'production')
-  ? '.jackhu.top'
-  : ''
