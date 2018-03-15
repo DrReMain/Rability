5c71b9d8cd6d7b43d9ddb842d545107d9f50c3ae
DrRemain <remain@divergers.com>
2018-03-15 14:36:23 +0800

    webpack dev.config

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -1,31 +1,34 @@
-const Express = require('express')
-const webpack = require('webpack')
+var Express = require('express')
+var webpack = require('webpack')
 
-const config = require('../src/config')
-const webpackConfig = require('./dev.config')
-const compiler = webpack(webpackConfig)
+var config = require('../src/config')
+var webpackConfig = require('./dev.config')
+var compiler = webpack(webpackConfig)
 
-const host = config.host || 'localhost'
-const port = (Number(config.port) + 1) || 3001
-const serverOptions = {
-  contentBase: `http://${host}:${port}`,
+var host = config.host || 'localhost'
+var port = (Number(config.port) + 1) || 3001
+var serverOptions = {
+  contentBase: 'http://' + host + ':' + port,
   quiet: true,
   noInfo: true,
   hot: true,
   inline: true,
   lazy: false,
   publicPath: webpackConfig.output.publicPath,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
+  headers: { 'Access-Control-Allow-Origin': '*' },
 }
 
-const app = new Express()
+var app = new Express()
 
 app.use(require('webpack-dev-middleware')(compiler, serverOptions))
 app.use(require('webpack-hot-middleware')(compiler))
 
-app.listen(port, function(err) {
-  if (err) console.error(err)
-  console.info('==> 🌎  Webpack development server listening on port %s', port)
+app.listen(port, function onAppListening(err) {
+  if (err) {
+    console.error(err)
+  }
+  else {
+    console.info(
+      '==> 🚧  Webpack development server listening on port %s', port)
+  }
 })
