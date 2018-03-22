a2627679a25f1d803a2935240a81706ac38f235c
DrRemain <remain@divergers.com>
2018-03-22 16:19:21 +0800

    // TODO aboutredux api-server

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,25 +4,29 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { renderRoutes } from 'react-router-config';
 import Helmet from 'react-helmet';
+import { increment } from '../../redux/modules/demo';
 
 const helmetConf = {
   titleTemplate: 'App: %s',
   meta: [{ charset: 'utf-8' }]
 };
 
-@connect(state => ({ num: state.demo }))
+@connect(state => ({ demo: state.demo }), { increment })
 @withRouter
 export default class App extends Component {
   static propTypes = {
-    route: PropTypes.objectOf(PropTypes.any).isRequired
+    route: PropTypes.objectOf(PropTypes.any).isRequired,
+    demo: PropTypes.objectOf(PropTypes.number).isRequired,
+    increment: PropTypes.func.isRequired
   };
 
   render() {
-    const { route } = this.props;
+    const { route, demo, increment: add } = this.props;
     return (
       <div className="demo">
         <Helmet {...helmetConf} />
-        <h1>App</h1>
+        <h1>{demo.count}</h1>
+        <button onClick={add}>+</button>
         <section>{renderRoutes(route.routes)}</section>
       </div>
     );
